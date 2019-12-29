23af2ec928c461ce75b8499b18b48dc2c2801bc5
sp2_hybrid <1171891648@qq.com>
2019-12-29 16:45:28 +0800

    eval commit

---
+++ eval/test_eval.py
@@ -0,0 +1,232 @@
+from __future__ import print_function
+import torch
+import torch.nn as nn
+import torch.backends.cudnn as cudnn
+from torch.autograd import Variable
+import sys
+sys.path.append('..')
+from data import VOC_ROOT, VOCAnnotationTransform, VOCDetection, BaseTransform
+from data import VOC_CLASSES as labelmap
+import torch.utils.data as data
+
+from ssd import build_ssd
+
+
+import os
+import time
+import argparse
+import numpy as np
+import pickle
+import cv2
+
+if sys.version_info[0] == 2:
+    import xml.etree.cElementTree as ET
+else:
+    import xml.etree.ElementTree as ET
+
+
+def str2bool(v):
+    return v.lower() in ("yes", "true", "t", "1")
+
+
+parser = argparse.ArgumentParser(
+    description='Single Shot MultiBox Detector Evaluation')
+parser.add_argument('--trained_model',
+                    default='../weights/ssd300_COCO_70000.pth', type=str,
+                    help='Trained state_dict file path to open')
+parser.add_argument('--save_folder', default='./', type=str,
+                    help='File path to save results')
+parser.add_argument('--confidence_threshold', default=0.01, type=float,
+                    help='Detection confidence threshold')
+parser.add_argument('--top_k', default=5, type=int,
+                    help='Further restrict the number of predictions to parse')
+parser.add_argument('--cuda', default=True, type=str2bool,
+                    help='Use cuda to train model')
+#train时不用写VOC_ROOT,test时为'/home/songpeng/ssd/ssd.pytorch.2'
+parser.add_argument('--voc_root', default=VOC_ROOT,
+                    help='Location of VOC root directory')
+parser.add_argument('--cleanup', default=True, type=str2bool,
+                    help='Cleanup and remove results files following eval')
+
+args = parser.parse_args()
+
+if not os.path.exists(args.save_folder):
+    os.mkdir(args.save_folder)
+
+if torch.cuda.is_available():
+    if args.cuda:
+        torch.set_default_tensor_type('torch.cuda.FloatTensor')
+    if not args.cuda:
+        print("WARNING: It looks like you have a CUDA device, but aren't using \
+              CUDA.  Run with --cuda for optimal eval speed.")
+        torch.set_default_tensor_type('torch.FloatTensor')
+else:
+    torch.set_default_tensor_type('torch.FloatTensor')
+
+annopath = os.path.join(args.voc_root, 'VOC', 'Annotations', '%s.xml')
+imgpath = os.path.join(args.voc_root, 'VOC', 'JPEGImages', '%s.jpg')
+imgsetpath = os.path.join(args.voc_root, 'VOC', 'ImageSets',
+                          'Main', '{:s}.txt')
+YEAR = '2007'
+devkit_path = os.path.join(args.voc_root, 'eval')
+dataset_mean = (104, 117, 123)
+set_type = 'test'
+
+class Timer(object):
+    """A simple timer."""
+    def __init__(self):
+        self.total_time = 0.
+        self.calls = 0
+        self.start_time = 0.
+        self.diff = 0.
+        self.average_time = 0.
+
+    def tic(self):
+        # using time.time instead of time.clock because time time.clock
+        # does not normalize for multithreading
+        self.start_time = time.time()
+
+    def toc(self, average=True):
+        self.diff = time.time() - self.start_time
+        self.total_time += self.diff
+        self.calls += 1
+        self.average_time = self.total_time / self.calls
+        if average:
+            return self.average_time
+        else:
+            return self.diff
+        
+#解析xml文件
+def parse_rec(filename):
+    """ Parse a PASCAL VOC xml file """
+    tree = ET.parse(filename)
+    objects = []
+    for obj in tree.findall('object'):
+        obj_struct = {}
+        obj_struct['name'] = obj.find('name').text
+        #obj_struct['pose'] = obj.find('pose').text
+        #obj_struct['truncated'] = int(obj.find('truncated').text)
+        obj_struct['difficult'] = int(obj.find('difficult').text)
+        bbox = obj.find('bndbox')
+        obj_struct['bbox'] = [int(bbox.find('xmin').text) - 1,
+                              int(bbox.find('ymin').text) - 1,
+                              int(bbox.find('xmax').text) - 1,
+                              int(bbox.find('ymax').text) - 1]
+        objects.append(obj_struct)
+
+    return objects
+#创建目录'name/phase'用来存储结果文件
+def get_output_dir(name, phase):
+    """Return the directory where experimental artifacts are placed.
+    If the directory does not exist, it is created.
+    A canonical path is built using the name from an imdb and a network
+    (if not None).
+    """
+    filedir = os.path.join(name, phase)
+    if not os.path.exists(filedir):
+        os.makedirs(filedir)
+    return filedir
+
+def get_voc_results_file_template(image_set, cls):
+    # /data/VOCdevkitVOC/results/det_test_aeroplane.txt
+    if cls == 'core':
+        cls = '带电芯充电宝'
+    else:
+        cls = '不带电芯充电宝'
+    
+    filename = 'det_' + image_set + '_%s.txt' % (cls)
+    filedir = os.path.join(devkit_path, 'results')
+    if not os.path.exists(filedir):
+        os.makedirs(filedir)
+    path = os.path.join(filedir, filename)
+    return path
+
+def write_voc_results_file(all_boxes, dataset):
+    for cls_ind, cls in enumerate(labelmap):
+        print('Writing {:s} VOC results file'.format(cls))
+        #结果txt文件地址
+        filename = get_voc_results_file_template(set_type, cls)
+        
+        with open(filename, 'wt') as f:
+            for im_ind, index in enumerate(dataset.ids):
+                dets = all_boxes[cls_ind+1][im_ind]
+                if dets == []:
+                    continue
+                # the VOCdevkit expects 1-based indices
+                for k in range(dets.shape[0]):
+                    f.write('{:s} {:.3f} {:.1f} {:.1f} {:.1f} {:.1f}\n'.
+                            format(index[1], dets[k, -1],
+                                   dets[k, 0] + 1, dets[k, 1] + 1,
+                                   dets[k, 2] + 1, dets[k, 3] + 1))
+                    
+def test_net(save_folder, net, cuda, dataset, transform, top_k,
+             im_size=300, thresh=0.05):
+    num_images = len(dataset)
+    # all detections are collected into:
+    #    all_boxes[cls][image] = N x 5 array of detections in
+    #    (x1, y1, x2, y2, score)
+    all_boxes = [[[] for _ in range(num_images)]
+                 for _ in range(len(labelmap)+1)]
+
+    # timers
+    _t = {'im_detect': Timer(), 'misc': Timer()}
+    #output_dir = ssd300_120000/test
+    output_dir = get_output_dir('ssd300_120000', set_type)
+    det_file = os.path.join(output_dir, 'detections.pkl')
+
+    for i in range(num_images):
+        im, gt, h, w = dataset.pull_item(i)
+
+        x = Variable(im.unsqueeze(0))
+        if args.cuda:
+            x = x.cuda()
+        _t['im_detect'].tic()
+        detections = net(x).data
+        detect_time = _t['im_detect'].toc(average=False)
+
+        # skip j = 0, because it's the background class
+        for j in range(1, detections.size(1)):
+            dets = detections[0, j, :]
+            mask = dets[:, 0].gt(0.).expand(5, dets.size(0)).t()
+            dets = torch.masked_select(dets, mask).view(-1, 5)
+            if dets.size(0) == 0:
+                continue
+            boxes = dets[:, 1:]
+            boxes[:, 0] *= w
+            boxes[:, 2] *= w
+            boxes[:, 1] *= h
+            boxes[:, 3] *= h
+            scores = dets[:, 0].cpu().numpy()
+            cls_dets = np.hstack((boxes.cpu().numpy(),
+                                  scores[:, np.newaxis])).astype(np.float32,
+                                                                 copy=False)
+            all_boxes[j][i] = cls_dets
+
+        print('im_detect: {:d}/{:d} {:.3f}s'.format(i + 1,
+                                                    num_images, detect_time))
+
+    with open(det_file, 'wb') as f:
+        pickle.dump(all_boxes, f, pickle.HIGHEST_PROTOCOL)
+
+    print('Evaluating detections')
+    write_voc_results_file(all_boxes, dataset)
+
+                    
+if __name__ == '__main__':
+    # load net
+    num_classes = len(labelmap) + 1                      # +1 for background
+    net = build_ssd('test', 300, num_classes)            # initialize SSD
+    net.load_state_dict(torch.load(args.trained_model))
+    net.eval()
+    print('Finished loading model!')
+    # load data
+    dataset = VOCDetection(args.voc_root, [('2007', set_type)],
+                           BaseTransform(300, dataset_mean),
+                           VOCAnnotationTransform())
+    if args.cuda:
+        net = net.cuda()
+        cudnn.benchmark = True
+    # evaluation
+    test_net(args.save_folder, net, args.cuda, dataset,
+             BaseTransform(net.size, dataset_mean), args.top_k, 300,
+             thresh=args.confidence_threshold)
