00561ba559d95019627564ebb77634fad214eee0
sp2_hybrid <1171891648@qq.com>
2020-01-09 17:58:34 +0800

    添加模型并修改测试集路径

--- eval/test_eval.py
+++ eval/test_eval.py
@@ -32,7 +32,7 @@ def str2bool(v):
 parser = argparse.ArgumentParser(
     description='Single Shot MultiBox Detector Evaluation')
 parser.add_argument('--trained_model',
-                    default='../weights/ssd300_COCO_70000.pth', type=str,
+                    default='../weights/ssd300_COCO_115000.pth', type=str,
                     help='Trained state_dict file path to open')
 parser.add_argument('--save_folder', default='./', type=str,
                     help='File path to save results')
